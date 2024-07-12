a428ac08a1273768faec7fa6787a8605c93808f5
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2024-07-12 16:12:25 +0900

    いったんコードを整理

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,9 +3,7 @@ package com.example;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.treesitter.TSInputEncoding;
 import org.treesitter.TSLanguage;
@@ -19,7 +17,6 @@ public class Main {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         byte[] bytes = classloader.getResourceAsStream("main.js").readAllBytes();
         String codeContent = new String(bytes, StandardCharsets.UTF_8);
-        // System.out.println(codeContent);
 
         TSParser parser = new TSParser();
         TSLanguage javascript = new TreeSitterJavascript();
@@ -31,24 +28,6 @@ public class Main {
         var parts = new ByteArrayOutputStream();
         var result = traverse(rootNode, bytes, parts);
         System.out.println(new String(result));
-
-        //for (int i = 0; i < rootNode.getChildCount(); i++) {
-        //    var node = rootNode.getChild(i);
-        //    var type = node.getType();
-
-        //    if (type.equalsIgnoreCase("comment")) {
-        //        var startByte = node.getStartByte();
-        //        var endByte = node.getEndByte();
-        //        System.out.println(type);
-        //        System.out.println("startByte: " + startByte);                
-        //        System.out.println("endByte: " + endByte);
-
-        //        var commentBytes = Arrays.copyOfRange(bytes, startByte, endByte);
-        //        var comment = new String(commentBytes, StandardCharsets.UTF_8);
-
-        //        System.out.println(comment);
-        //    }
-        //}
     }
 
     public static byte[] traverse(TSNode node, byte[] code, ByteArrayOutputStream parts) throws IOException {
